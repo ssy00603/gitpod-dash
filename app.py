89750d7e0a607960fe753a46de9794efb386190f
Siyu Shen <siyushen@Siyus-MacBook-Pro.local>
2021-12-10 14:16:00 -0500

    add state name

--- app.py
+++ app.py
@@ -15,49 +15,52 @@ states = sorted(df.state.unique())
 days = ['last 7 days', 'last 14 days', 'last 30 days']
 
 app.layout = html.Div([
+    # Add title
     html.H2(children="Covid-19 Dashboard"),
 
+    # Add live date
     dcc.Interval(
         id='interval-component',
         interval=100000,
         n_intervals=0
     ),
 
-
+    # Add tabs
     dcc.Tabs(id="tabs-inline", value='tab-1', children=[
+        # Add tab of New Cases
         dcc.Tab(label='New Cases by States', value='tab-1', children=[
+            # Choose states 
             dcc.Dropdown(
                 id="dropdown",
                 options=[{"label": x, "value": x} for x in states],
                 value=states[0],
                 clearable=False,
             ),
-
+            # Choose day intervals
             dcc.Dropdown(
                 id="dropdown-days",
                 options=[{"label": x, "value": x} for x in days],
                 value=days[0],
                 clearable=False,
             ),
-
+            # Display bar plot
             dcc.Graph(id="bar-chart-new")
         ]),
 
+        # Add tab of Total Cases
         dcc.Tab(label='Total Cases', value='tab-2', children=[
+            # Display bar plot
             dcc.Graph(id="bar-chart-total")
         ]),
 
+        # Add tab of Deaths
         dcc.Tab(label='Deaths', value='tab-3', children=[
+            # Display bar plot
             dcc.Graph(id="bar-chart-death")
         ])]),
 
-        dcc.Graph(id="map-vaccine"),
-    # html.H4(children="States with Most New Cases Yestarday"),
-
-    # dash_table.DataTable(
-    # id='df_state_vaccine',
-    # columns=[{"name": i, "id": i} for i in ['state', 'new cases']],
-    # data=df.to_dict('records'))
+        # Display map
+        dcc.Graph(id="map-vaccine")
 
 ])
 
@@ -70,8 +73,12 @@ app.layout = html.Div([
 
 def bar_plot_days(n_clicks, states, days):
     df = pd.read_csv('https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-states.csv')
+    # Add previous cases column
     df['prev_cases'] = df.groupby('state')['cases'].shift().fillna(0)
+    # Add new cases column
     df['new cases'] = df['cases'] - df['prev_cases']
+
+    # Choose display day interval
     if days == 'last 7 days':
         df = df[df["state"] == states].iloc[-1:-8:-1][::-1]
     if days == 'last 14 days':
@@ -79,12 +86,12 @@ def bar_plot_days(n_clicks, states, days):
     else:
         df = df[df["state"] == states].iloc[-1:-31:-1][::-1]
 
+    # Plot line with bar plot
     fig = px.line(df, x="date", y="new cases")
     fig.add_bar(x=df["date"], y=df["new cases"], marker=dict(color="Blue", opacity=0.3), showlegend=False)
 
     fig.update_layout(
     plot_bgcolor="white",
-    # title="Number of Cases per State",
     xaxis_title="Date",
     yaxis_title="Number of New Cases"
     )
@@ -100,6 +107,7 @@ def bar_plot_total(n_clicks):
     df = pd.read_csv('https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-states.csv')
     df = df.groupby('date').sum().reset_index()
     
+    # Plot line with bar plot
     fig = px.line(df, x="date", y="cases")
     fig.add_bar(x=df["date"], y=df["cases"], marker=dict(color="Blue", opacity=0.3), showlegend=False)
 
@@ -120,6 +128,7 @@ def bar_plot_death(n_clicks):
     df = pd.read_csv('https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-states.csv')
     df = df.groupby('date').sum().reset_index()
     
+    # Plot line with bar plot
     fig = px.line(df, x="date", y="deaths")
     fig.add_bar(x=df["date"], y=df["deaths"], marker=dict(color="Blue", opacity=0.3), showlegend=False)
 
@@ -145,6 +154,7 @@ def map_vaccine(n):
     df_state_vaccine = df_state_code.merge(df_vaccine, left_on='State', right_on='location')
     df_state_vaccine = df_state_vaccine[['State', 'Abbreviation', 'total_vaccinations']]
 
+    # Plot map
     fig = px.choropleth(df_state_vaccine,
                         locations='Abbreviation',
                         color='total_vaccinations',
@@ -154,7 +164,15 @@ def map_vaccine(n):
                         labels={'total_vaccinations':'Number of Vaccinations'},
                         scope='usa')
 
+    # Hide line between states
     fig.update_traces(marker_line_width=0)
+
+    # Add State abbreviation
+    fig.add_scattergeo(locations=df_state_vaccine['Abbreviation'],
+                        locationmode='USA-states',
+                        text=df_state_vaccine['Abbreviation'],
+                        mode='text',
+                        hoverinfo='none')
 
     fig.update_layout(
         title={'text':'Total Vaccinations by State',
@@ -163,21 +181,5 @@ def map_vaccine(n):
             'x':0.5})
 
     return fig
-
-# @app.callback(
-#     Output('table', 'data'),
-#     Input('interval-component', 'n_intervals'))
-
-# def update_table(n):
-#     today = datetime.date.today()
-#     yestarday = str(today - datetime.timedelta(days=1))
-
-#     df = pd.read_csv('https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-states.csv')
-#     df['prev_cases'] = df.groupby('state')['cases'].shift().fillna(0)
-#     df['new cases'] = df['cases'] - df['prev_cases']
-
-#     df_yes = df[df['date'] == yestarday]
-#     df_yes = df_yes[['state', 'new cases']].sort_values(by=['new cases'], ascending=False).head()
-#     return df_yes.to_dict('records')
 
 app.run_server(debug=True, host="0.0.0.0")
