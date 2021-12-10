48dd9b72482305bac6a34dd5c98b0d15e23bdaf7
Siyu Shen <siyushen@Siyus-MacBook-Pro.local>
2021-12-10 09:31:47 -0500

    add states with most new cases

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 import dash
 import pandas as pd
+import datetime
+
 import plotly.graph_objects as go
 import dash_core_components as dcc
 import dash_html_components as html
@@ -12,14 +14,18 @@ app = dash.Dash(__name__)
 df = pd.read_csv('https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-states.csv')
 states = sorted(df.state.unique())
 
+
 app.layout = html.Div([
     html.H2(children="Covid-19 Dashboard"),
+
+    html.H4(children="New Reported Cases"),
+
     dcc.Interval(
         id='interval-component',
-        interval=1*1000,
+        interval=100000,
         n_intervals=0
     ),
-     
+
     dcc.Dropdown(
         id="dropdown",
         options=[{"label": x, "value": x} for x in states],
@@ -27,11 +33,14 @@ app.layout = html.Div([
         clearable=False,
     ),
 
-    dcc.Graph(id="bar-chart")
-    # dash_table.DataTable(
-    # id='table',
-    # columns=[{"name": i, "id": i} for i in df.columns],
-    # data=df.to_dict('records'))
+    dcc.Graph(id="bar-chart"),
+
+    html.H4(children="States with Most New Cases Yestarday"),
+
+    dash_table.DataTable(
+    id='table',
+    columns=[{"name": i, "id": i} for i in ['state', 'new cases']],
+    data=df.to_dict('records'))
 
 ])
 
@@ -45,9 +54,10 @@ def bar_plot(n_clicks, states):
     df = pd.read_csv('https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-states.csv')
     df['prev_cases'] = df.groupby('state')['cases'].shift().fillna(0)
     df['new cases'] = df['cases'] - df['prev_cases']
-    df_last_30 = df[df["state"] == states].iloc[-1:-31:-1][::-1]
 
-    fig = px.bar(df_last_30, x="date", y="new cases")
+    df_last_30 = df[df["state"] == states].iloc[-1:-100:-1][::-1]
+
+    fig = px.bar(df_last_30, x="date", y="new cases", opacity=0.5)
     
     fig.update_layout(
     plot_bgcolor="white",
@@ -55,7 +65,23 @@ def bar_plot(n_clicks, states):
     xaxis_title="Date",
     yaxis_title="Number of New Cases"
     )
-
     return fig
+
+
+@app.callback(
+    Output('table', 'data'),
+    Input('interval-component', 'n_intervals'))
+
+def update_table(n):
+    today = datetime.date.today()
+    yestarday = str(today - datetime.timedelta(days=1))
+
+    df = pd.read_csv('https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-states.csv')
+    df['prev_cases'] = df.groupby('state')['cases'].shift().fillna(0)
+    df['new cases'] = df['cases'] - df['prev_cases']
+
+    df_yes = df[df['date'] == yestarday]
+    df_yes = df_yes[['state', 'new cases']].sort_values(by=['new cases'], ascending=False).head()
+    return df_yes.to_dict('records')
 
 app.run_server(debug=True, host="0.0.0.0")
